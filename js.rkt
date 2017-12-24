902a4e0109f328e3871df15fc223aa6c30a7e545
zaoqi <zaomir@outlook.com>
2017-12-24 16:05:30 +0800

    re

--- js.rkt
+++ js.rkt
@@ -14,144 +14,158 @@
 ;;  You should have received a copy of the GNU Affero General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (define ++ string-append)
-(define (**var x) (symbol->string x)) ; Bug
-(define (**return x) (++ "return "x))
-(define (**lambda args body)
-  (++ "(function("(%**lam-arg args)"){"
-      (**top body)
-      "})"))
-(define (**top xs)
-  (if (null? (cdr xs))
-      (++ (car xs)";")
-      (++ (car xs)";"(**top (cdr xs)))))
+
 (define (%**lam-arg args)
   (cond
     [(symbol? args) (++ (**var args)"...")]
     [(null? args) ""]
     [(null? (cdr args)) (**var (car args))]
     [else (++ (**var (car args))","(%**lam-arg (cdr args)))]))
-(define (%add%between xs k) (foldr string-append "" (add-between xs k)))
-(define (%**app xs) (%add%between xs ","))
-(define (**apply* f xs) (++ f"("(%**app xs)")"))
-(define (*js-typeof x) (++ "(typeof "x")"))
-(define (*procedure? x) (++ "(typeof "x"=='function')"))
-(define (*string? x) (++ "(typeof "x"=='string')"))
-(define (*number? x) (++ "(typeof "x"=='number')"))
-(define (*boolean? x) (++ "(typeof "x"=='boolean')"))
-(define (*object/vector? x) (++ "(typeof "x"=='object')"))
-(define (*char? x) (++ "(function(x){return typeof x=='string'&&x.length==1;})("x")"))
-(define (*is-a? x t) (++ "("x" instanceof "t")"))
-(define (**c-for s1 s2 s3 xs) (++ "for("s1";"s2";"s3"){"(**top xs)"}"))
-(define (**for-vector i v xs)
-  (let ([s (gensym)] [o (gensym)])
-    (++ (**define o v)
-        (**c-for (**define s 0) (*< (**var s) (*length (**var o))) (**++ (**var s))
-                 (cons (**define i (**var s)) xs)))))
+(define (ADDbetweenSTRING xs k) (foldr string-append "" (add-between xs k)))
+(define ((*%* k) xs) (++ "("(ADDbetweenSTRING xs k)")"))
+(define ((%*xfx f) x y) (++ "("x f y")"))
+
+(define (**var x) (symbol->string x)) ; Bug
+(define (**top xs)
+  (if (null? (cdr xs))
+      (++ (car xs)";")
+      (++ (car xs)";"(**top (cdr xs)))))
 (define (**define s x) (++ "var "(**var s)"="x))
 (define (**define-undefined s) (++ "var "(**var s)))
-(define (*length v) (++ v".length"))
-(define (**++ v) (++ v"++"))
-(define *undefined "undefined")
-(define (*undefined? x) (++ "("x"===null)"))
+(define (**set! v x) (++ (**var v)"="x))
 
-(define ((*%* k) xs) (++ "("(%add%between xs k)")"))
+(define (**return x) (++ "return "x))
+(define (**lambda args body)
+  (++ "(function("(%**lam-arg args)"){"
+      (**top body)
+      "})"))
+(define (%**app xs) (ADDbetweenSTRING xs ","))
+(define (**apply* f xs) (++ f"("(%**app xs)")"))
+(define (*procedure? x) (++ "(typeof "x"=='function')"))
+
+(define (*raise x) (++ "throw "x))
+(define (**try xs e ys)
+  (++ "try{"(**top xs)"}catch("(**var e)"){"(**top ys)"}"))
+
+(define (*boolean? x) (++ "(typeof "x"=='boolean')"))
+(define *false "false")
+(define *true "true")
+(define *eq? (%*xfx "==="))
+(define *not-eq? (%*xfx "!=="))
 (define (*or* xs) (*%* "||"))
 (define (*and* xs) (*%* "&&"))
+(define (*not x) (++ "(!"x")"))
+(define (**if-boolean b xb yb) (++ "if("b"){"(**top xb)"}else{"(**top yb)"}"))
+(define (**if b xb yb) (**if-boolean (*not-eq? b *false) xb yb))
+(define (*if-boolean b x y) (++ "("b"?"x":"y")"))
+(define (*if b x y) (*if-boolean (*not-eq? b *false) x y))
+
+(define (*string? x) (++ "(typeof "x"=='string')"))
+(define *string-append* (*%* "+"))
+(define (*string-ref s k) (++ s".charAt("k")"))
+(define (*string-length s) (++ s".length"))
+
+(define (*number? x) (++ "(typeof "x"=='number')"))
 (define *+* (*%* "+"))
 (define *-* (*%* "-"))
 (define *** (*%* "*"))
 (define */* (*%* "/"))
-(define ((%*xfx f) x y) (++ "("x f y")"))
-(define *eq? (%*xfx "==="))
 (define *= *eq?)
-(define *not-eq? (%*xfx "!=="))
-(define *js-eq? (%*xfx "=="))
-(define *js-not-eq? (%*xfx "!="))
 (define *< (%*xfx "<"))
 (define *> (%*xfx ">"))
 (define *<= (%*xfx "<="))
 (define *>= (%*xfx ">="))
 
-(define (*vector? x) (*is-a? x "Array"))
+(define %*r1-s (gensym))
+(define %*r1 (**var %*r1-s))
+(define %*r2 (**var (gensym)))
+(define (**@ o k) (++ o"."(**var k)))
+(define (**@= o k v) (++ o"."(**var k)"="v))
+(define (*object-ref o k) (++ o"["k"]"))
+(define (**object* xs) (++ "{"(ADDbetweenSTRING (map (λ (x) (++ (**var (first x))":"(second x))) xs) ",")"}"))
+(define (**: o k xs) (++ o"."(**var k)"("(%**app xs)")"))
+(define (*object/vector? x) (++ "(typeof "x"=='object')"))
+(define (*object-set! o k x) (++ o"["k"]="x))
+(define (**for-object i v xs)
+  (++ "var "%*r1"="v";"
+      "for(var "%*r2" in "v"){var "i"="%*r2";"(**top xs)"}"))
+(define (*vector? x) (++ "("x" instanceof Array)"))
 (define (*vector* xs) (++ "["(%**app xs)"]"))
 (define (*vector-set! v k x) (++ v"["k"]="x))
 (define (*vector-ref v k) (++ v"["k"]"))
-(define (*append xs ys) (++ xs".concat("ys")"))
+(define (*vector-append* xs) (++ (car xs)".concat("(%**app (cdr xs))")"))
+(define (*vector-length x) (++ x".length"))
+(define (**for-vector i v xs)
+  (++ "var "%*r1"="v";"
+      "for(var "%*r2"=0;"%*r2"<"%*r1".length;"%*r2"++){var "i"="%*r2";"(**top xs)"}"
+      ))
 
-(define (**js-for-in s o xs) (++ "for(var "(**var s)" in "o"){"(**top xs)"}"))
-(define (**js-if b xb yb) (++ "if("b"){"(**top xb)"}else{"(**top yb)"}"))
-(define (**if b xb yb) (**js-if (*not-eq? b *false) xb yb))
-(define *false "false")
-(define *true "true")
-(define (*js-if b x y) (++ "("b"?"x":"y")"))
-(define (*if b x y) (*js-if (*not-eq? b *false) x y))
-(define (**set! v x) (++ (**var v)"="x))
-(define (*object-set! o k x) (++ o"["k"]="x))
-(define (**/ o k) (++ o"."(**var k)))
-(define (**/= o k v) (++ o"."(**var k)"="v))
-(define (*object-ref o k) (++ o"["k"]"))
-(define (**object* xs) (++ "{"(%add%between (map (λ (x) (++ (**var (first x))":"(second x))) xs) ",")"}"))
-(define (**: o k xs) (++ o"."(**var k)"("(%**app xs)")"))
+
+(define *undefined "undefined")
+(define (*undefined? x) (++ "("x"===null)"))
+
 (define (**number x) (number->string (exact->inexact x)))
 (define (**string x) (format "~s" x))
-(define (*new* x xs) (++ "new "x"("(%**app xs)")"))
-(define %**struct-t (gensym))
-(define %**struct-t-v (**var %**struct-t))
 (define (**struct pred new fs)
   (let ([k (**var (gensym))])
     (++ "var "k"=function(){};"
         (**define new
                   (**lambda fs
                             (cons
-                             (**define %**struct-t (*new* k '()))
+                             (**define %*r1-s (++ "new "k"()"))
                              (append
-                              (map (λ (f) (**/= %**struct-t-v f (**var f))) fs)
-                              (list (**return %**struct-t-v))))))
+                              (map (λ (f) (**@= %*r1 f (**var f))) fs)
+                              (list (**return %*r1))))))
         ";"
         (**define pred
-                  (**lambda (list %**struct-t)
-                            (list (**return (*is-a? %**struct-t-v k))))))))
+                  (**lambda (list %*r1-s)
+                            (list (**return (++ "("%*r1" instanceof "k")"))))))))
 
 (define %*exp*fs
   (hash
-   'js-typeof *js-typeof
+   'return **return
+   'begin (λ xs (ADDbetweenSTRING xs ";"))
    'procedure? *procedure?
-   'string? *string?
+
+   'raise *raise
+
    'boolean? *boolean?
-   'number? *number?
-   'char? *char?
-   'object/vector? *object/vector?
-   'vector (λ xs (*vector* xs))
-   'vector? *vector?
-   'vector-set! *vector-set!
-   'vector-ref *vector-ref
-   'append *append
-   'undefined? *undefined?
-   'is-a? *is-a?
-   'length *length
+   'eq? *eq?
+   'not-eq? *not-eq?
    'or (λ xs (*or* xs))
    'and (λ xs (*and* xs))
+   'not *not
+   'if-boolean *if-boolean
+   'if *if
+
+   'string? *string?
+   'string-append (λ xs (*string-append* xs))
+   'string-ref *string-ref
+   'string-length *string-length
+
+   'number? *number?
    '+ (λ xs (*+* xs))
    '- (λ xs (*-* xs))
    '* (λ xs (*** xs))
    '/ (λ xs (*/* xs))
+   '= *=
    '< *<
    '> *>
    '<= *<=
    '>= *>=
-   'eq? *eq?
-   '= *=
-   'not-eq? *not-eq?
-   'js-eq? *js-eq?
-   'js-not-eq? *js-not-eq?
-   'if *if
-   'js-if *js-if
-   'object-set! *object-set!
-   'object-ref *object-ref
-   'new (λ (f . xs) (*new* f xs))
 
-   'return **return
-   'begin (λ xs (%add%between xs ";"))
+   'object-ref *object-ref
+   'object/vector? *object/vector?
+   'object-set! *object-set!
+   'vector? *vector?
+   'vector (λ xs (*vector* xs))
+   'vector-set! *vector-set!
+   'vector-ref *vector-ref
+   'vector-append *vector-append*
+   'vector-length *vector-length
+
+   'undefined *undefined
+   'undefined? *undefined?
    ))
    
 (define (*exp x)
@@ -165,20 +179,25 @@
                [(or (eq? f 'λ) (eq? f 'lambda)) (**lambda (car xs) (map *exp (cdr xs)))]
                [(eq? f 'define) (**define (first xs) (*exp (second xs)))]
                [(eq? f 'define-undefined) (**define-undefined (first xs))]
-               [(eq? f 'js-for-in) (**js-for-in (car xs) (*exp (cadr xs)) (map *exp (cddr xs)))]
-               [(eq? f 'js-if/do) (**js-if (*exp (first xs)) (map *exp (second xs)) (map *exp (third xs)))]
-               [(eq? f 'if/do) (**if (*exp (first xs)) (map *exp (second xs)) (map *exp (third xs)))]
                [(eq? f 'set!) (**set! (first xs) (*exp (second xs)))]
-               [(eq? f '/) (**/ (*exp (first xs)) (second xs))]
-               [(eq? f '/=) (**/= (*exp (first xs)) (second xs) (*exp (third xs)))]
-               [(eq? f ':) (**: (*exp (car xs)) (cadr xs) (map *exp (cddr xs)))]
-               [(eq? f 'struct) (**struct (first xs) (second xs) (third xs))]
+               [(eq? f 'try) (**try (map *exp (first xs)) (second xs) (map *exp (third xs)))]
+
+               [(eq? f 'if-boolean/do) (**if-boolean (*exp (first xs)) (map *exp (second xs)) (map *exp (third xs)))]
+               [(eq? f 'if/do) (**if (*exp (first xs)) (map *exp (second xs)) (map *exp (third xs)))]
+
+               [(eq? f '@) (**@ (*exp (first xs)) (second xs))]
+               [(eq? f '@=) (**@= (*exp (first xs)) (second xs) (*exp (third xs)))]
                [(eq? f 'object) (**object* (map (λ (x) (list (first x) (*exp (second x)))) xs))]
+               [(eq? f ':) (**: (*exp (car xs)) (cadr xs) (map *exp (cddr xs)))]
+
+               [(eq? f 'for-object) (**for-object (car xs) (*exp (cadr xs)) (cddr xs))]
+               [(eq? f 'for-vector) (**for-vector (car xs) (*exp (cadr xs)) (cddr xs))]
+
+               [(eq? x 'undefined) *undefined]
+
+               [(eq? f 'struct) (**struct (first xs) (second xs) (third xs))]
                [else (**apply* (*exp f) (map *exp xs))]))))]
-    [(symbol? x)
-     (cond
-       [(eq? x 'undefined) *undefined]
-       [else (**var x)])]
+    [(symbol? x) (**var x)]
     [(number? x) (**number x)]
     [(eq? x #t) *true]
     [(eq? x #f) *false]
